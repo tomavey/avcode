3c927b97a82720a45ea2879b3dc714e9d406a5c2
tomavey <tomavey@outlook.com>
2025-01-06 07:27:14 -0500

    DEPLOYED ON 01-06-25 7:26 AM TDA

--- app.vue
+++ app.vue
@@ -6,5 +6,11 @@
 
 <script setup>
 const { settingsObj, fetchSettings } = useSettings();
-onMounted(fetchSettings);
+onMounted(() => {
+  fetchSettings();
+  console.log(
+    "%c====> DEPLOYED ON 01-06-25 7:26 AM TDA <====",
+    "color: white; background: green;"
+  );
+});
 </script>
