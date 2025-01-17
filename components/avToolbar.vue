fb29fc2d965870af88d0fb92e6a40c92570d1cc9
tomavey <tomavey@outlook.com>
2025-01-17 11:48:39 -0500

    make logo link to "/"

--- components/avToolbar.vue
+++ components/avToolbar.vue
@@ -5,7 +5,7 @@
         density="compact"
         @click="toggleDrawer"
       ></v-app-bar-nav-icon>
-      <div>
+      <div @click="router.push('/')">
         <v-img src="@/assets/avcode3.png" :width="175" class="cursor-pointer" />
       </div>
       <v-spacer />
@@ -19,4 +19,5 @@
 <script setup>
 const { toggleDrawer } = useNav();
 const user = useSupabaseUser();
+const router = useRouter();
 </script>
