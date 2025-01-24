a8d951b3108eb826a0bd3098dae5266204d0bfe2
tomavey <tomavey@outlook.com>
2025-01-24 08:43:52 -0500

    ?

--- pages/calendar.vue
+++ pages/calendar.vue
@@ -49,110 +49,105 @@
   </v-dialog>
 </template>
 
-<script>
-import { useDate } from "vuetify";
+<script setup>
+const supabase = useSupabaseClient();
 
-export default {
-  data: () => ({
-    type: "month",
-    types: ["month", "week", "day"],
-    weekday: [0, 1, 2, 3, 4, 5, 6],
-    weekdays: [
-      { title: "Sun - Sat", value: [0, 1, 2, 3, 4, 5, 6] },
-      { title: "Mon - Sun", value: [1, 2, 3, 4, 5, 6, 0] },
-      { title: "Mon - Fri", value: [1, 2, 3, 4, 5] },
-      { title: "Mon, Wed, Fri", value: [1, 3, 5] },
-    ],
-    value: [new Date()],
-    events: [],
-    dialog: false,
-    eventsTest: [
-      {
-        title: "Weekly Meeting",
-        start: new Date("2025-01-23 09:00"),
-        end: new Date("2025-01-24 10:00"),
-      },
-      {
-        title: `Thomas' Birthday`,
-        start: new Date("2025-01-10"),
-        end: new Date("2025-01-10"),
-        allDay: true,
-      },
-      {
-        title: "Mash Potatoes",
-        start: new Date("2025-01-09 12:30"),
-        end: new Date("2025-01-09 15:30"),
-      },
-    ],
-    savedEvents: [],
-    colors: [
-      "blue",
-      "indigo",
-      "deep-purple",
-      "cyan",
-      "green",
-      "orange",
-      "grey darken-1",
-    ],
-    titles: [
-      "Meeting",
-      "Holiday",
-      "PTO",
-      "Travel",
-      "Event",
-      "Birthday",
-      "Conference",
-      "Party",
-    ],
-  }),
-  mounted() {
-    const adapter = useDate();
-    // this.getSavedEvents();
-    this.getEvents();
-    document.addEventListener("DOMContentLoaded", () => {
-      const chipContentDiv = document.querySelector(".v-btn");
-      if (chipContentDiv) {
-        chipContentDiv.addEventListener("click", () => {
-          console.log("Chip content clicked!");
-          // Your custom logic here
-        });
-      }
-    });
+const type = ref("month");
+const types = ["month", "week", "day"];
+const weekday = ref([0, 1, 2, 3, 4, 5, 6]);
+const weekdays = [
+  { title: "Sun - Sat", value: [0, 1, 2, 3, 4, 5, 6] },
+  { title: "Mon - Sun", value: [1, 2, 3, 4, 5, 6, 0] },
+  { title: "Mon - Fri", value: [1, 2, 3, 4, 5] },
+  { title: "Mon, Wed, Fri", value: [1, 3, 5] },
+];
+const value = ref([new Date()]);
+const events = ref([]);
+const dialog = ref(false);
+const eventsTest = [
+  {
+    title: "Weekly Meeting",
+    start: new Date("2025-01-23 09:00"),
+    end: new Date("2025-01-24 10:00"),
   },
-  methods: {
-    async getEvents() {
-      const supabase = useSupabaseClient();
-      const { data, error } = await supabase.from("calendar_dates").select("*");
-      if (error) {
-        console.error(error);
-      } else {
-        this.events = this.convertEvents(data);
-      }
-    },
-
-    convertEvents(data) {
-      const convertedEvents = data.map((event) => ({
-        ...event,
-        start: new Date(event.start),
-        end: new Date(event.end),
-        allDay: event.allDay || false,
-        color: event.color || this.colors[this.rnd(0, this.colors.length - 1)],
-        title: event.title || "Untitled Event",
-        end: new Date(event.end),
-      }));
-      return convertedEvents;
-    },
-
-    getEventColor(event) {
-      return event.color;
-    },
-    rnd(a, b) {
-      return Math.floor((b - a + 1) * Math.random()) + a;
-    },
-
-    clickclick(id) {
-      console.log("clickclick", id);
-    },
+  {
+    title: `Thomas' Birthday`,
+    start: new Date("2025-01-10"),
+    end: new Date("2025-01-10"),
+    allDay: true,
   },
+  {
+    title: "Mash Potatoes",
+    start: new Date("2025-01-09 12:30"),
+    end: new Date("2025-01-09 15:30"),
+  },
+];
+const savedEvents = ref([]);
+const colors = [
+  "blue",
+  "indigo",
+  "deep-purple",
+  "cyan",
+  "green",
+  "orange",
+  "grey darken-1",
+];
+const titles = [
+  "Meeting",
+  "Holiday",
+  "PTO",
+  "Travel",
+  "Event",
+  "Birthday",
+  "Conference",
+  "Party",
+];
+
+const getEvents = async () => {
+  const supabase = useSupabaseClient();
+  const { data, error } = await supabase.from("calendar_dates").select("*");
+  if (error) {
+    console.error(error);
+  } else {
+    events.value = convertEvents(data);
+  }
 };
+
+const convertEvents = (data) => {
+  const convertedEvents = data.map((event) => ({
+    ...event,
+    start: new Date(event.start),
+    end: new Date(event.end),
+    allDay: event.allDay || false,
+    color: event.color || colors[rnd(0, colors.length - 1)],
+    title: event.title || "Untitled Event",
+  }));
+  return convertedEvents;
+};
+
+const getEventColor = (event) => {
+  return event.color;
+};
+
+const rnd = (a, b) => {
+  return Math.floor((b - a + 1) * Math.random()) + a;
+};
+
+const clickclick = (id) => {
+  console.log("clickclick", id);
+};
+
+onMounted(() => {
+  const adapter = useDate();
+  getEvents();
+  document.addEventListener("DOMContentLoaded", () => {
+    const chipContentDiv = document.querySelector(".v-btn");
+    if (chipContentDiv) {
+      chipContentDiv.addEventListener("click", () => {
+        console.log("Chip content clicked!");
+        // Your custom logic here
+      });
+    }
+  });
+});
 </script>
